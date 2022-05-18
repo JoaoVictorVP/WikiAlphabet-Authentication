954ce5ed5b3b4233e7e32df0ea8df646fadea9ab
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 13:38:58 -0300

    app user

---
+++ Authentication.Server/XIdentity/Core/Models/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using Authentication.Server.XIdentity.Contracts;
+using Authentication.Shared;
+using Authentication.Shared.Contracts;
+
+namespace Authentication.Server.XIdentity.Core.Models;
+
+public class AppUser : IUser
+{
+    private readonly User _user;
+
+    public AppUser(User user)
+    {
+        _user = user;
+    }
+
+    public string Id { get => _user.Id; set => _user.Id = value; }
+    public string Username { get => _user.Username; set => _user.Username = value; }
+    public string Email { get => _user.Email; set => _user.Email = value; }
+    public string PasswordHash => CryptoUtils.HashPassword(_user.Password, Env.Salt);
+
+    public IEnumerable<UserRole> GetAllRoles() => throw new NotImplementedException();
+    public UserRole GetRole(string roleName) => throw new NotImplementedException();
+    public void AddRole(UserRole role) => throw new NotImplementedException();
+    public void RemoveRole(string roleName) => throw new NotImplementedException();
+}
