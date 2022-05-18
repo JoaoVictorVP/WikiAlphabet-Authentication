dfa9151bed84d507f5fcc079a69cb0de3144157a
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 14:44:20 -0300

    Setup

--- Authentication.Server/Configuration/ApplicationExtensions.cs
+++ Authentication.Server/Configuration/ApplicationExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Authentication.Shared;
 using Authentication.Shared.Contracts.Validators;
 using Authentication.Shared.Core.Validators;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Authentication.Server.Configuration;
 
@@ -8,7 +11,30 @@ public static class ApplicationExtensions
 {
     public static void AddAuthServices(this IServiceCollection services)
     {
-        
+        var key = new SymmetricSecurityKey(Env.Secret);
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = true;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new ()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = key,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+    }
+
+    public static void ConfigureAuth(this WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
     }
 
     public static void AddValidators(this IServiceCollection services)
