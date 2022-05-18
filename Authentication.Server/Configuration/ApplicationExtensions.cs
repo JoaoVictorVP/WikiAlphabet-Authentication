57c679db7631af77850bc479d862dc7990d2706b
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 12:03:08 -0300

    add

---
+++ Authentication.Server/Configuration/ApplicationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using AspNetCore.Identity.LiteDB;
+using Authentication.Shared;
+
+namespace Authentication.Server.Configuration;
+
+public static class ApplicationExtensions
+{
+    public static void AddAuthServices(this IServiceCollection services)
+    {
+        
+    }
+
+    public static void ConfigureIdentity(this IServiceCollection services)
+    {
+        services.AddIdentity<User, Role>(options =>
+        {
+            options.User = new()
+            {
+                RequireUniqueEmail = true
+            };
+            options.Password = new()
+            {
+                RequireDigit = false,
+                RequiredLength = 6,
+                RequiredUniqueChars = 0,
+                RequireLowercase = false,
+                RequireNonAlphanumeric = false,
+                RequireUppercase = false
+            };
+            options.Lockout = new()
+            {
+                AllowedForNewUsers = true,
+                MaxFailedAccessAttempts = 5,
+                DefaultLockoutTimeSpan = TimeSpan.FromDays(5)
+            };
+        })
+        .AddUserStore<LiteDbUserStore<ApplicationUser>>();
+    }
+}
