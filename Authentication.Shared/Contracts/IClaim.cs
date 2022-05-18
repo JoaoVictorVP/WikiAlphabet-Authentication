950bfea0df4236a30c362f52ed75ddfe5e5d6456
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 13:37:41 -0300

    add

---
+++ Authentication.Shared/Contracts/IClaim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Shared.Contracts;
+
+public interface IClaim
+{
+    string Name { get; }
+    ClaimLevel Level { get; }
+}
+
+public enum ClaimLevel
+{
+    ReadOnly,
+    WriteOnly,
+    WriteAndRead
+}
