96622383aeba1083c6ce4af862ce76f2352a3c13
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 12:03:20 -0300

    add

---
+++ Authentication.Server/Env.cs
@@ -0,0 +1,10 @@
+﻿namespace Authentication.Server;
+
+public static class Env
+{
+    public static readonly byte[] Secret = Convert.FromBase64String(Environment.GetEnvironmentVariable("WIKI_AUTH_SECRET")
+        ?? throw new NotImplementedException("Need to define an environment variable called WIKI_AUTH_SECRET"));
+
+    public static readonly byte[] Salt = Convert.FromBase64String(Environment.GetEnvironmentVariable("WIKI_AUTH_SALT")
+        ?? throw new NotImplementedException("Need to define an environment variable called WIKI_AUTH_SALT"));
+}
