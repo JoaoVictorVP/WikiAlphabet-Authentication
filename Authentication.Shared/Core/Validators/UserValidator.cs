c689c78a945e41d1edd38d790875c92043b84e60
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 13:53:19 -0300

    moving types to right place and adding contracts/implementations

---
+++ Authentication.Shared/Core/Validators/UserValidator.cs
@@ -0,0 +1,20 @@
+﻿using Authentication.Shared.Contracts;
+using Authentication.Shared.Contracts.Validators;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Shared.Core.Validators;
+
+public class UserValidator : AbstractValidator<User>, IUserValidator
+{
+    public UserValidator()
+    {
+        RuleFor(x => x.Name).NotNull().NotEmpty().MinimumLength(3);
+        RuleFor(x => x.Username).NotNull().NotEmpty().MinimumLength(6);
+        RuleFor(x => x.Email).EmailAddress();
+    }
+}
