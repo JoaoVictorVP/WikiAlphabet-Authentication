77219d431ffb43e6a2843c45227cd3458187405e
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 15:36:51 -0300

    app user now backing correctly underlying user

--- Authentication.Server/XIdentity/Core/Models/AppUser.cs
+++ Authentication.Server/XIdentity/Core/Models/AppUser.cs
@@ -1,21 +1,27 @@
 ﻿using Authentication.Server.XIdentity.Contracts;
 using Authentication.Shared;
 using Authentication.Shared.Contracts;
+using System.Runtime.Serialization;
 
 namespace Authentication.Server.XIdentity.Core.Models;
 
 public class AppUser : IUser
 {
-    private readonly User _user;
+    private User _user;
+    public User User { get => _user; set => _user = value; }
 
     public AppUser(User user)
     {
         _user = user;
     }
 
+    [IgnoreDataMember]
     public string Id { get => _user.Id; set => _user.Id = value; }
+    [IgnoreDataMember]
     public string Username { get => _user.Username; set => _user.Username = value; }
+    [IgnoreDataMember]
     public string Email { get => _user.Email; set => _user.Email = value; }
+    [IgnoreDataMember]
     public string PasswordHash => CryptoUtils.HashPassword(_user.Password, Env.Salt);
 
     public IEnumerable<UserRole> GetAllRoles() => _user.UserRoles;
