2b5e8751b7d1489c6dbdc60349bc396937f3d962
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 15:36:28 -0300

    .

--- Authentication.Server/XIdentity/Contracts/Managers/IUserManager.cs
+++ Authentication.Server/XIdentity/Contracts/Managers/IUserManager.cs
@@ -4,7 +4,7 @@ namespace Authentication.Server.XIdentity.Contracts.Managers;
 
 public interface IUserManager<TUser> where TUser : IUser
 {
-    Task<bool> Register(IUser user);
+    Task<bool> Register(TUser user);
     Task<bool> DeleteAccount(string userId);
     Task<bool> ChangePassword(string userId, string oldPassword, string newPassword);
     Task<bool> ChangeEmail(string userId, string newEmail);
