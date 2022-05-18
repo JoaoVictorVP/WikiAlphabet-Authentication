8ba431e94d86e3b01e848e61b444bf6d79eb223f
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 13:38:29 -0300

    manager contracts

---
+++ Authentication.Server/XIdentity/Contracts/Managers/IUserManager.cs
@@ -0,0 +1,12 @@
+﻿using Authentication.Shared;
+
+namespace Authentication.Server.XIdentity.Contracts.Managers;
+
+public interface IUserManager<TUser> where TUser : IUser
+{
+    bool Register(IUser user);
+    bool DeleteAccount(string userId);
+    bool ChangePassword(string userId, string oldPassword, string newPassword);
+    bool ChangeEmail(string userId, string newEmail);
+    bool IsValidPassword(string userId, string passwordHash);
+}
