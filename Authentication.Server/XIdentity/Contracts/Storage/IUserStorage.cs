4e7ec8d1519800c50fc3e77a9c17c58c69df1120
João Victor <joao.pacheco@wikialphabet.com>
2022-05-18 15:03:52 -0300

    storage for generalizing interfaces with common storage needs factory in case of future cache needs

---
+++ Authentication.Server/XIdentity/Contracts/Storage/IUserStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace Authentication.Server.XIdentity.Contracts.Storage;
+
+public interface IUserStorage
+{
+    Task<IUser?> GetUser(string id);
+    Task<IUser?> GetUserByUsername(string username);
+    Task<IUser?> GetUserByEmail(string email);
+    IAsyncEnumerable<IUser> GetUsersByRole(string roleName);
+    Task AddUser(IUser user);
+    Task RemoveUser(string id);
+}
