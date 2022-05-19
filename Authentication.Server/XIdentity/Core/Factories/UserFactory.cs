1324de9261b4d42bf1bff30dc2f076197d957c09
João Victor <joao.pacheco@wikialphabet.com>
2022-05-19 10:15:30 -0300

    now with update user

--- Authentication.Server/XIdentity/Core/Factories/UserFactory.cs
+++ Authentication.Server/XIdentity/Core/Factories/UserFactory.cs
@@ -18,5 +18,6 @@ public class UserFactory : IUserFactory
     public Task<IUser?> GetUserByEmail(string email) => _userRepository.GetUserByEmail(email);
     public IAsyncEnumerable<IUser> GetUsersByRole(string roleName) => _userRepository.GetUsersByRole(roleName);
     public Task AddUser(IUser user) => _userRepository.AddUser(user);
+    public Task UpdateUser(string id, IUser user) => _userRepository.UpdateUser(id, user);
     public Task RemoveUser(string id) => _userRepository.RemoveUser(id);
 }
