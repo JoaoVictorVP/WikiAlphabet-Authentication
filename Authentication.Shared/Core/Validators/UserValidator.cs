ae3aee54434aeacfbd53d83b975c85ed0d3b3171
João Victor <joao.pacheco@wikialphabet.com>
2022-05-20 11:30:39 -0300

    .

--- Authentication.Shared/Core/Validators/UserValidator.cs
+++ Authentication.Shared/Core/Validators/UserValidator.cs
@@ -13,8 +13,10 @@ public class UserValidator : AbstractValidator<IUser>, IUserValidator
 {
     public UserValidator()
     {
-        RuleFor(x => x.Name).NotNull().NotEmpty().MinimumLength(3);
-        RuleFor(x => x.Username).NotNull().NotEmpty().MinimumLength(6);
+        RuleFor(x => x.Name).NotNull().NotEmpty().MinimumLength(1);
+        RuleFor(x => x.Username).NotNull().NotEmpty().MinimumLength(1);
         RuleFor(x => x.Email).EmailAddress();
+        RuleFor(x => x.CreatedDate).GreaterThan(DateTime.MinValue).LessThan(DateTime.MaxValue);
+        RuleFor(x => x.Deleted).Equal(true);
     }
 }
