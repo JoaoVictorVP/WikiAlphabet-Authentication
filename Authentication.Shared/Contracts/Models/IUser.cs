211c5cc606689df6e79543c1be11fb511bd5b71c
João Victor <joao.pacheco@wikialphabet.com>
2022-05-20 10:25:54 -0300

    make User concrete model derive from new interface IUser

---
+++ Authentication.Shared/Contracts/Models/IUser.cs
@@ -0,0 +1,18 @@
+﻿namespace Authentication.Shared.Contracts;
+
+public interface IUser
+{
+    string Id { get; set; }
+    string Name { get; set; }
+    string Username { get; set; }
+    string Email { get; set; }
+    string Password { get; set; }
+
+    List<UserRole> UserRoles { get; set; }
+
+    bool Active { get; set; }
+    DateTime CreatedDate { get; set; }
+
+    bool Deleted { get; set; }
+    DateTime? DeletedDate { get; set; }
+}
