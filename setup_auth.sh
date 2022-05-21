ec9d8e4196364cb7edf266ac0d3eaaf3e4fa0ddd
João Victor <joao.pacheco@wikialphabet.com>
2022-05-21 19:35:07 -0300

    better setup files

--- setup_auth.sh
+++ setup_auth.sh
@@ -9,6 +9,6 @@ setx WIKI_AUTH_SALT $WIKI_AUTH_SECRET
 echo "Sua chave secreta foi gerada e armazenada na variável de ambiente WIKI_AUTH_SALT"
 
 export WIKI_AUTH_BCRYPT_WORK_FACTOR
-WIKI_AUTH_BCRYPT_WORK_FACTOR="10"
+WIKI_AUTH_BCRYPT_WORK_FACTOR="11"
 setx WIKI_AUTH_BCRYPT_WORK_FACTOR  $WIKI_AUTH_BCRYPT_WORK_FACTOR
 echo "A dificuldade do hasher de senhas foi gerada e armazenada na variável de ambiente WIKI_AUTH_BCRYPT_WORK_FACTOR"
