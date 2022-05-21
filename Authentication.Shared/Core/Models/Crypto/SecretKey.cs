7902315f6087a1af81e8d598a1179e41eecc4f61
João Victor <joao.pacheco@wikialphabet.com>
2022-05-21 15:28:14 -0300

    impl Secret key

--- Authentication.Shared/Core/Models/Crypto/SecretKey.cs
+++ Authentication.Shared/Core/Models/Crypto/SecretKey.cs
@@ -9,12 +9,16 @@ namespace Authentication.Shared.Core.Models.Crypto;
 
 public class SecretKey : ICryptoArgsWithKey
 {
+    byte[] _key = Array.Empty<byte>();
     public int KeySize => 0;
 
-    public void Key(string base64Key) => throw new NotImplementedException();
-    public void Key(ReadOnlySpan<byte> key) => throw new NotImplementedException();
-    public byte[] GetKey() => throw new NotImplementedException();
-    public void GetKey(Span<byte> destKey) => throw new NotImplementedException();
+    public void Key(string base64Key) => _key = base64Key.FromBase64();
+    public void Key(ReadOnlySpan<byte> key) => _key = key.ToArray();
+    public byte[] GetKey() => _key;
+    public void GetKey(Span<byte> destKey) => _key.CopyTo(destKey);
+
+    public T Get<T>(string name) => name.ToString() is "key" or "secret" or "secretKey" ?
+        (T)(object)_key : throw new NotImplementedException($"No implementation for {name}");
+
     public void Generic(string name, object value) => throw new NotImplementedException();
-    public T Get<T>(string name) => throw new NotImplementedException();
 }
