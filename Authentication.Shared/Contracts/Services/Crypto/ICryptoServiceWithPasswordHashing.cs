35cae465e2ff03bdb7011a44e3dad07f24e3e115
João Victor <joao.pacheco@wikialphabet.com>
2022-05-21 17:03:41 -0300

    for password hashing

---
+++ Authentication.Shared/Contracts/Services/Crypto/ICryptoServiceWithPasswordHashing.cs
@@ -0,0 +1,10 @@
+﻿using Authentication.Shared.Contracts.Models;
+
+namespace Authentication.Shared.Contracts.Services.Crypto;
+
+public interface ICryptoServiceWithPasswordHashing<TArgsSalt, TArgsDifficulty> : ICryptoService
+    where TArgsSalt : ICryptoArgsWithSalt
+    where TArgsDifficulty : ICryptoArgsWithDifficulty
+{
+    string HashPassword(string password, TArgsSalt argsSalt, TArgsDifficulty argsDifficulty);
+}
