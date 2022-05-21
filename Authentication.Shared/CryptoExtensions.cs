e6d791bbd12f8ed9b355e66f5af0789fdae31b55
João Victor <joao.pacheco@wikialphabet.com>
2022-05-21 15:23:55 -0300

    to (base64/hex) from (base64/hex) methods added

--- Authentication.Shared/CryptoExtensions.cs
+++ Authentication.Shared/CryptoExtensions.cs
@@ -2,5 +2,9 @@
 
 public static class CryptoExtensions
 {
-    
+    public static string ToBase64(this ReadOnlySpan<byte> bytes) => Convert.ToBase64String(bytes);
+    public static byte[] FromBase64(this string base64) => Convert.FromBase64String(base64);
+
+    public static string ToHex(this ReadOnlySpan<byte> bytes) => Convert.ToHexString(bytes);
+    public static byte[] FromHex(this ReadOnlySpan<char> hex) => Convert.FromHexString(hex);
 }
