54adcc09555f8447cc9588e67333e6d8cd623dd5
João Victor <joao.pacheco@wikialphabet.com>
2022-05-21 15:18:02 -0300

    adding secret key class and crypto extensions for latter usage

---
+++ Authentication.Shared/Core/Models/Crypto/SecretKey.cs
@@ -0,0 +1,20 @@
+﻿using Authentication.Shared.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Shared.Core.Models.Crypto;
+
+public class SecretKey : ICryptoArgsWithKey
+{
+    public int KeySize => 0;
+
+    public void Key(string base64Key) => throw new NotImplementedException();
+    public void Key(ReadOnlySpan<byte> key) => throw new NotImplementedException();
+    public byte[] GetKey() => throw new NotImplementedException();
+    public void GetKey(Span<byte> destKey) => throw new NotImplementedException();
+    public void Generic(string name, object value) => throw new NotImplementedException();
+    public T Get<T>(string name) => throw new NotImplementedException();
+}
---
+++ Authentication.Shared/CryptoExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Authentication.Shared;
+
+public static class CryptoExtensions
+{
+    
+}
