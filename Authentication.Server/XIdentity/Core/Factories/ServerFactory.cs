776230cdb6df5fb45164087e0154db3663aee5b4
João Victor <joao.pacheco@wikialphabet.com>
2022-05-22 17:42:12 -0300

    adding server factory

---
+++ Authentication.Server/XIdentity/Core/Factories/ServerFactory.cs
@@ -0,0 +1,40 @@
+﻿using Authentication.Server.XIdentity.Contracts;
+using Authentication.Server.XIdentity.Contracts.Factories;
+using Authentication.Server.XIdentity.Contracts.Repositories;
+
+namespace Authentication.Server.XIdentity.Core.Factories;
+
+public class ServerFactory<TServer> : IServerFactory<TServer> where TServer : IServer
+{
+    private readonly IServerRepository<TServer> _serverRepository;
+
+    public ServerFactory(IServerRepository<TServer> serverRepository)
+    {
+        _serverRepository = serverRepository;
+    }
+
+    public async Task AddServerAsync(TServer server)
+    {
+        await _serverRepository.AddServerAsync(server);
+    }
+
+    public async Task UpdateServerAsync(TServer server)
+    {
+        await _serverRepository.UpdateServerAsync(server);
+    }
+
+    public async Task RemoveServerAsync(TServer server)
+    {
+        await _serverRepository.RemoveServerAsync(server);
+    }
+
+    public async Task<TServer?> GetServerAsync(string id)
+    {
+        return await _serverRepository.GetServerAsync(id);
+    }
+
+    public async Task<TServer?> GetServerByEmailAsync(string email)
+    {
+        return await _serverRepository.GetServerByEmailAsync(email);
+    }
+}
